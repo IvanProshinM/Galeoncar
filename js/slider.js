e6cf4f79ab57b6446f45198f023d0a18b23e9143
ivan.proshin.m <ivan.proshin.m@gmail.com>
2021-12-27 20:45:19 +0300

    Сделал еще пару блоков, осталось доделать слайдер и footer  с картой

--- js/slider.js
+++ js/slider.js
@@ -58,4 +58,50 @@ $(document).ready(function () {
 
     });
 })
+/*$(".arrow").click(function() {
+    $(this).toggleClass("open");
+});*/
+/*$(document).ready(function() {
+    $('.conditions-part__link').click(function (event) {
+      $(this).toggleClass('active').next().slideToggle(300).toggleClass("open")
 
+    });
+});*/
+
+$(document).ready(function() {
+    $('.conditions-part__link').click(function (event) {
+        $(this).next().slideToggle(300)
+        $(this).find('.arrow').toggleClass("open")
+    });
+
+});
+$(document).ready(function () {
+    $('.reviews-part__slider').slick({
+        arrows: true,
+        dots: true,           /*точечки внизу (видно/не видно)*/
+        adaptiveHeight: false, /*подстройка высоты изображения под размер блока*/
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        speed: 500,
+        easing: 'ease',        /*тип анимации при смене слайда linear - default*/
+        infinite: true,       /* бесконечно листать слайды или нет*/
+        initialSlide: 1,       /* с какого слайда начинать*/
+        autoplay: false,        /* автоматическое пролистывание*/
+        autoplaySpeed: 1500,  /* скорость автоматического пролистывания*/
+        pauseOnFocus: true,    /* пауза, когда слайдер активен*/
+        pauseOnHover: true,    /* пауза, когда наводишь курсор на слайдер*/
+        pauseOnDotsHover: true,/* пауза, когда наводишь курсор на точки*/
+        draggable: true,       /*пролистывание мышкой вкл/выкл*/
+        swipe: true,           /*пролистывание пальцем на телефоне вкл/выкл*/
+        touchThreshold: 5,     /*расстояние, которое надо преодолеть для перелистывания слайдера на телефоне*/
+        touchMove: true,       /*двишать слайдер мышкой вкл/выкл*/
+        waitForAnimate: true,   /*ожидание окончания анимации перед следующим переключением слайда*/
+        centerMode: true,
+        variableWidth: false,     /*подстройка ширины блока под размер изображения*/
+        rows: 1,               /*ряды слайдера*/
+        slidesPerRow: false,  /*количество рядов слайдера*/
+        vertical: false,        /*вертикальный слайдер (поменяй в css flex в классе, где у тебя слайдер!)*/
+        verticalSwiping: false  /*очевидно же...*/
+
+    });
+})
